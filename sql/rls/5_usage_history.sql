9a831a9ebcb0a44b2905c853fec45429fb28f3ca
Bryan Banks <bry5an@gmail.com>
2025-12-24 11:26:36 -0800

    update rls for usage_history

--- sql/rls/5_usage_history.sql
+++ sql/rls/5_usage_history.sql
@@ -1,20 +1,24 @@
 create policy "bot can read usage_history"
 on public.usage_history
 for select
+to anon, authenticated
 using (true);
 
 create policy "bot can insert usage_history"
 on public.usage_history
 for insert
+to anon, authenticated
 with check (true);
 
 create policy "bot can update usage_history"
 on public.usage_history
 for update
+to anon, authenticated
 using (true)
 with check (true);
 
 create policy "bot can delete usage_history"
 on public.usage_history
 for delete
+to anon, authenticated
 using (true);
